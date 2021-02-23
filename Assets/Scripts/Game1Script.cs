b7ce417a29172939c9af79fa275bd528fee24c28
ninja-n8 <slug.sam8@gmail.com>
2021-02-23 15:50:51 -0800

    Scene Transition Files
    Add base scenes and scene transition scripts

---
+++ Assets/Scripts/Game1Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game1Script : MonoBehaviour {
+    [SerializeField] private Button buttonHiLow;
+    [SerializeField] private Button buttonBlackJack;
+    [SerializeField] private Button buttonFiveCard;
+    [SerializeField] private Button buttonSevenCard;
+
+    // Start is called before the first frame update
+    void Start () {
+        buttonHiLow.onClick.AddListener(() => {
+            SceneTransitioner.Instance.LoadScene(2);
+        });
+        buttonBlackJack.onClick.AddListener(() => {
+            SceneTransitioner.Instance.LoadScene(3);
+        });
+        buttonFiveCard.onClick.AddListener(() => {
+            SceneTransitioner.Instance.LoadScene(4);
+        });
+        buttonSevenCard.onClick.AddListener(() => {
+            SceneTransitioner.Instance.LoadScene(5);
+        });
+    }
+}
---
+++ Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadingScript : MonoBehaviour {
+
+    [SerializeField] private TMP_Text progressText;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float animSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start () {
+        canvas.gameObject.SetActive(false);
+
+        SceneTransitioner.OnStartedLoading += () => {
+            StartCoroutine(FadeIn());
+        };
+
+        SceneTransitioner.OnProgressUpdated += f => {
+            progressText.text = $"{(f * 100f)}%";
+        };
+
+        SceneTransitioner.OnDone += () => {
+            StartCoroutine(FadeOut());
+            SceneTransitioner.Finish();
+        };
+    }
+
+    private IEnumerator FadeIn() {
+        canvas.gameObject.SetActive(true);
+        var alpha = 0f;
+        while (alpha < 1f) {
+            canvasGroup.alpha = Mathf.SmoothStep(0f, 1f, alpha);
+            alpha += Time.deltaTime * animSpeed;
+            yield return null;
+        }
+        canvasGroup.alpha = 1f;
+    }
+
+    private IEnumerator FadeOut () {
+        var alpha = 0f;
+        while (alpha < 1f) {
+            var a = Mathf.SmoothStep(1f, 0f, alpha);
+            canvasGroup.alpha = a;
+            alpha += Time.deltaTime * animSpeed;
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+        canvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
